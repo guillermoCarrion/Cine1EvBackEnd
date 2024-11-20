8efe8a43684869763bd9e369f107f5d379f3d0c5
Izarbe02 <zarbix02@gmail.com>
2024-11-20 17:45:45 +0100

    Cambios en peliculas

--- CineApi/Models/Pelicula.cs
+++ CineApi/Models/Pelicula.cs
@@ -10,17 +10,29 @@ public class Pelicula
     public string Sinopsis { get; set; } 
     public string Imagen { get; set; } 
    public int IdSala {get; set;}
+
+   public string Duracion {get; set;}
+
+   public string Genero {get; set;}
+
+   public string  FechaEstreno {get; set;}
+
+   public string Pegi {get; set;}
     public List<Sesion> sesiones {get; set;} = new List<Sesion>();
 
 
 
- public Pelicula(int id , string nombre, string director, string sinopsis, string imagen, int idsala) {
+ public Pelicula(int id , string nombre, string director, string sinopsis, string imagen, int idsala , string  duracion , string genero , string fechaEstreno, string pegi) {
         Id = id;
         Nombre = nombre;
         Director = director;
          Sinopsis = sinopsis;
          Imagen = imagen;
          IdSala = idsala;
+         Duracion = duracion; 
+         Genero = genero;
+         FechaEstreno = fechaEstreno;
+         Pegi = pegi; 
     }
 
  //Pelicula.InicializarDatos();
