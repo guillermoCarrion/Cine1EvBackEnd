e0a6dcc5be58a7533b4d35d7e1266f3c46aae0ef
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-12-03 18:26:53 +0100

    docker back

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirOrigenLocal", policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500") // Permitir el acceso desde tu frontend
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://34.225.199.154:5500") // Permitir el acceso desde tu frontend
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
@@ -26,12 +26,12 @@ var app = builder.Build();
 app.UseCors("PermitirOrigenLocal");
 
 
-// Configure the HTTP request pipeline.
-/*if (app.Environment.IsDevelopment())
-{*/
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 
 app.UseHttpsRedirection();
@@ -40,6 +40,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 PeliculaController.InicializarDatos();
-app.Run();
+app.Run("http://0.0.0.0:7141");
 
 //http://localhost:5165/swagger/index.html
